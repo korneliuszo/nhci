ab736096f87694cb4e362fcd65c72c7e91d7ed35
Korneliusz Osmenda <korneliuszo@gmail.com>
2021-04-26 16:15:31 +0200

    First verilator usage (apio and system needed)

--- fw/top.v
+++ fw/top.v
@@ -1,24 +1,25 @@
 module top(
 	input wire CLK,
-	output [0:0] D,
+	output [0:0] D
 	);
 
 	wire clk_26;
-
+/* verilator lint_off PINMISSING */
 	SB_PLL40_CORE #(
 `include "pll.v"
 	)  SB_PLL40_CORE_inst (
 		.RESETB(1'b1),
 		.BYPASS(1'b0),
-		.PLLOUTCORE(clk_26),
+		.PLLOUTGLOBAL(clk_26),
 		.REFERENCECLK(CLK)
 	);
+/* verilator lint_on PINMISSING */
 
 	initial begin
 		D[0] = 0;
 	end
 
-	always @(posedge clk26)
+	always @(posedge clk_26)
 	begin
 		D[0] <= ~D[0];
 	end
