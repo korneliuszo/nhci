76538dbe4958e12af8a0d2a77c02a81f017dbafc
Korneliusz Osmenda <korneliuszo@gmail.com>
2021-04-28 21:16:28 +0200

    Add configuration tuple

--- fw/rom.v
+++ fw/rom.v
@@ -9,10 +9,10 @@ module rom(
 	reg [7:0] mem [0:255];
 	
 	assign DDIR = !(REGSELB | OE) & !(A[9]);
-	assign D = DDIR ? mem[A[8:1]] : 8'bZ;
+	assign D = mem[A[8:1]];
 	
 initial begin
-	mem[0] = 8'hff;
+	$readmemh("attr.list", mem);
 end
 
 endmodule
